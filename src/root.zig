562f7d8dae6f53e85ba00bda8c8b691a201d55f5
BR <bjorn.roberg@gmail.com>
2026-01-25 23:56:33 +0100

    wip

--- src/root.zig
+++ src/root.zig
@@ -25,22 +25,25 @@ pub const BinaryFileKind = enum {
     elf,
     macho,
     pe,
-};
-pub const Endianess = enum {
-    little,
-    big,
+    ape,
 };
 
+const Endian = std.builtin.Endian;
 const ElfHint = struct {
     bitness: u8,
-    endianess: Endianess,
+    endianess: Endian,
+};
+const MachoHint = struct {
+    bitness: u8,
+    endianess: Endian,
 };
 
 const Stage0ParseResult = union(BinaryFileKind) {
     unknown,
     elf: ElfHint,
-    macho: u8, // TBD MachoHint
+    macho: MachoHint,
     pe: u8, // TBD PeHint
+    ape: u8, // TBD ApeHint
 };
 
 pub fn bufferedRead(path: []const u8, buffer: []u8, max_length: usize) !void {
@@ -69,4 +72,49 @@ test "bufferedRead: base case 2 - read ELF file" {
     try bufferedRead("testing/assets/bian", buf[0..], prefix_length);
     try expect(0x7F == buf[0]);
     try expect(mem.eql(u8, "ELF", buf[1..4]));
+}
+
+pub fn detectFormat(buffer: []u8) Stage0ParseResult {
+    if (buffer.len > 4 and buffer[0] == 0x7F and mem.eql(u8, "ELF", buffer[1..4])) {
+        // ELF header fields:
+        //   EI_CLASS (32/64) at offset 4
+        //   EI_DATA (endian) at offset 5
+        const ei_class = buffer[4]; // 1 = 32-bit, 2 = 64-bit
+        const ei_data = buffer[5]; // 1 = little, 2 = big endian
+        return Stage0ParseResult{ .elf = ElfHint{ .bitness = if (ei_class == 1) 32 else 64, .endianess = if (ei_data == 1) .little else .big } };
+    }
+
+    if (buffer.len > 4) {
+        // https://en.wikipedia.org/wiki/Mach-O
+        // For big-endian binaries (as in, the architecture uses big endian),
+        // magic number for 32-bit code is 0xfeedface while the magic number for 64-bit architectures is 0xfeedfacf.
+        // For little-endian binaries,
+        // it will be 0xcefaedfe for 32-bit and 0xcffaedfe for 64-bit.
+        // These latter two are just the former but with inverted endianness.
+        const magic = mem.readInt(u32, buffer[0..4], .big);
+        // std.debug.print("{x}\n", .{magic});
+        if (magic == 0xfeedface) {
+            // 32 BE
+            return Stage0ParseResult{ .macho = MachoHint{ .bitness = 32, .endianess = .big } };
+        } else if (magic == 0xfeedfacf) {
+            // 64 BE
+            return Stage0ParseResult{ .macho = MachoHint{ .bitness = 64, .endianess = .big } };
+        } else if (magic == 0xcefaedfe) {
+            // 32 LE
+            return Stage0ParseResult{ .macho = MachoHint{ .bitness = 32, .endianess = .little } };
+        } else if (magic == 0xcffaedfe) {
+            // 64 LE
+            return Stage0ParseResult{ .macho = MachoHint{ .bitness = 64, .endianess = .little } };
+        }
+    }
+
+    return Stage0ParseResult{ .unknown = undefined };
+}
+
+test "macho.1" {
+    var buf: [prefix_length]u8 = @splat(0);
+    try bufferedRead("/home/jani/devel/binary-samples/MachO-OSX-x64-ls", buf[0..], prefix_length);
+    const presult = detectFormat(buf[0..]);
+    // std.debug.print("{}\n", .{presult});
+    try expect(presult.macho.bitness == 64);
 }
